b3721f0b10b57a9ea717d8b18df6e3924301c089
adoumadje <adoumadjecaleb@gmail.com>
2026-06-08 08:53:34 +0200

    need to use specifications

--- backend/src/main/java/com/jobhuntinger/job/repository/JobRepository.java
+++ backend/src/main/java/com/jobhuntinger/job/repository/JobRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -15,24 +16,33 @@ import java.util.UUID;
 
 @Repository
 public interface JobRepository extends CrudRepository<Job, Long> {
+//    @Query("""
+//            SELECT j
+//            FROM Job j
+//            WHERE j.user = :user
+//              AND (
+//                    :keyword IS NULL
+//                    OR LOWER(j.jobTitle) LIKE LOWER(CONCAT('%', :keyword, '%'))
+//                    OR LOWER(j.companyName) LIKE LOWER(CONCAT('%', :keyword, '%'))
+//              )
+//              AND (
+//                    :toDate IS NULL
+//                    OR j.createdAt < :toDate
+//              )
+//            """)
     @Query("""
-            SELECT j
-            FROM Job j
-            WHERE j.user = :user
-              AND (
-                    :keyword IS NULL
-                    OR LOWER(j.jobTitle) LIKE LOWER(CONCAT('%', :keyword, '%'))
-                    OR LOWER(j.companyName) LIKE LOWER(CONCAT('%', :keyword, '%'))
-              )
-              AND (
-                    :toDate IS NULL
-                    OR j.createdAt < :toDate
-              )
-            """)
+    SELECT j
+    FROM Job j
+    WHERE j.user = :user
+      AND (
+            :toDate IS NULL
+            OR j.createdAt < :toDate
+      )
+    """)
     Page<Job> searchJobs(
-            User user,
-            String keyword,
-            LocalDateTime toDate,
+            @Param("user") User user,
+            @Param("keyword") String keyword,
+            @Param("toDate") LocalDateTime toDate,
             Pageable pageable);
 
     Optional<Job> findByJobPublicId(UUID jobPublicId);
