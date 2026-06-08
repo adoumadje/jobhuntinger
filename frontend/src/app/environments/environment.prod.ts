dd875e9b336470318739f4d225cbda0937865482
adoumadje <adoumadjecaleb@gmail.com>
2026-06-08 17:07:33 +0200

    everything fine - release v1

---
+++ frontend/src/app/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    GOOGLE_AUTH_CLIENT_ID: "65808850161-le04f4u2bclf8jijd3o8uqd4973q1f36.apps.googleusercontent.com",
+    BACKEND_BASE_URL: "",
+    TOKEN_NAME: "google_id_token"
+}
